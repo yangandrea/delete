a961aa38a9e4ff5aca7d584b9cc432dc973d6920
itismeucci <yang.andrea@itismeucci.com>
2024-03-11 11:25:16 +0100

    ciao

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
+require_once  __DIR__ . '/controller/AlunniController.php';
+require_once  __DIR__ . '/controller/ApiAlunniController.php';
+
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$app->get('/alunni', 'AlunniController:index');
+$app->get('/alunni/{nome}', 'AlunniController:alunno');
+$app->get('/json/alunni','ApiAlunniController:all' );
+$app->get('/json/alunni/{nome}','ApiAlunniController:search' );
 
 $app->run();
